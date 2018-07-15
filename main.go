6f28b8d289cbc72544dcb47b0c43617a0d12ce3b
Cody Tipton <webmaster@iminshell.com>
2018-07-15 01:10:46 -0500

    Lot going on here, and it's very late.  May roll back later.

--- main.go
+++ main.go
@@ -4,6 +4,9 @@ import (
     "crypto/tls"
     "log"
     "net/http"
+    "io/ioutil"
+    "fmt"
+    "net/url"
 )
 
 func redirectTLS(w http.ResponseWriter, r *http.Request) {
@@ -25,6 +28,7 @@ func index(w http.ResponseWriter, req *http.Request) {
 
 
 func main() {
+	go getkey()
 	go http.ListenAndServe(":80", http.HandlerFunc(redirectTLS))
     mux := http.NewServeMux()
     mux.HandleFunc("/", index) //func(w http.ResponseWriter, req *http.Request) {
@@ -49,5 +53,44 @@ func main() {
         TLSNextProto: make(map[string]func(*http.Server, *tls.Conn, http.Handler), 0),
     }
     log.Fatal(srv.ListenAndServeTLS("/etc/ssl/certs/server.crt", "/etc/ssl/certs/server.key"))
+
+}
+
+func getkey() {
+	b, err := ioutil.ReadFile("/srv/apikey")
+	if err != nil {
+		fmt.Print(err)
+	}
+	apikey := string(b)
+}
+
+
+func getzipurl() {
+		u, err := url.Parse("http://bing.com/search?q=dotnet")
+	if err != nil {
+		log.Fatal(err)
+	}
+	u.Scheme = "https"
+	u.Host = "api.ziprecruiter.com"
+	u.Path = "jobs/"
+	q := u.Query()
+	q.Set("v1?search", getjob())
+	u.RawQuery = q.Encode()
+	fmt.Println(u)
+}
+
+func getjob() {
+	Println("golang")
+}
+
+
+func zipsearch() {
+	fmt.Println("Starting...")
+//	jsdonData := map[
+
+	go getzipurl()
+	http.NewRequest("GET", "golang")
+	
+	//"https://api.ziprecruiter.com/jobs/v1?search=" + job + "&location=" + locale + "&radius_miles=" + miles + "&days_ago=" + days_ago + "&jobs_per_page=10&page=1&api_key=" + apikey)
 
 }
