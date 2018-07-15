b9e0f2debbb56baa685005d88aba2db2baa736a8
Cody Tipton <webmaster@iminshell.com>
2018-07-15 10:39:58 -0500

    day 2

--- main.go
+++ main.go
@@ -28,7 +28,6 @@ func index(w http.ResponseWriter, req *http.Request) {
 
 
 func main() {
-	go getkey()
 	go http.ListenAndServe(":80", http.HandlerFunc(redirectTLS))
     mux := http.NewServeMux()
     mux.HandleFunc("/", index) //func(w http.ResponseWriter, req *http.Request) {
@@ -56,40 +55,56 @@ func main() {
 
 }
 
-func getkey() {
-	b, err := ioutil.ReadFile("/srv/apikey")
-	if err != nil {
-		fmt.Print(err)
-	}
-	apikey := string(b)
-}
 
+const apikey = "juu7m6ckf9z3rf2g5a8hwc9577amh26e" 
+
+//func getkey() {
+//	b, err := ioutil.ReadFile("/srv/apikey")
+//	if err != nil {
+//		fmt.Print(err)
+//	}
+//	var apikey string = string(b)
+//}
 
 func getzipurl() {
-		u, err := url.Parse("http://bing.com/search?q=dotnet")
+	u, err := url.Parse("api.ziprecruiter.com/jobs/")
 	if err != nil {
-		log.Fatal(err)
+	log.Fatal(err)
 	}
 	u.Scheme = "https"
 	u.Host = "api.ziprecruiter.com"
-	u.Path = "jobs/"
+	u.Path = "jobs/v1"
 	q := u.Query()
-	q.Set("v1?search", getjob())
+	q.Set("search", job)
+	q.Add("&location", locale)
+	q.Add("&radius_miles", miles)
+	q.Add("&days_ago", days_ago)
+	q.Add("&jobs_per_page", perp)
+	q.Add("&api_key", apikey)
 	u.RawQuery = q.Encode()
 	fmt.Println(u)
 }
 
-func getjob() {
-	Println("golang")
-}
 
+
+
+const job = "golang"
+const locale = "Michigan"
+const miles = "25"
+const days_ago = "30"
+const perp = "20"
 
 func zipsearch() {
 	fmt.Println("Starting...")
-//	jsdonData := map[
-
-	go getzipurl()
-	http.NewRequest("GET", "golang")
+	search := http.NewRequest("GET", string getzipurl)
+	client := &http.Client{}
+	response, err := client.Do(search)
+	if err != nil {
+		fmt.Printf("HTTP req failed with error %s\n", err)
+	} else {
+		data, _ := ioutil.ReadAll(response.Body)
+		fmt.Println(string(data))
+	}
 	
 	//"https://api.ziprecruiter.com/jobs/v1?search=" + job + "&location=" + locale + "&radius_miles=" + miles + "&days_ago=" + days_ago + "&jobs_per_page=10&page=1&api_key=" + apikey)
 
